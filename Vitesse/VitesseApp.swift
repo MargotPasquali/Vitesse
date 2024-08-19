61043f3a096b614c0dd53c8feb111ae1b7dce614
MargotPasquali <margotpasquali1@gmail.com>
2024-08-19 16:35:44 +0200

    initial commit

---
+++ Vitesse/VitesseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitesseApp.swift
+//  Vitesse
+//
+//  Created by Margot Pasquali on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct VitesseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
